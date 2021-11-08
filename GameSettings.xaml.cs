60c0c5184165289e5fa8290e5492b433dff8f65e
Vassterak <vaclavhorcicka@outlook.cz>
2021-11-08 17:11:47 +0100

    Simple game menu with user input validation

---
+++ GameSettings.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace WPF_Game_Gorillas
+{
+    /// <summary>
+    /// Interaction logic for GameSettings.xaml
+    /// </summary>
+    public partial class GameSettings : Window
+    {
+        public GameSettings()
+        {
+            InitializeComponent();
+        }
+
+        private static readonly Regex _lettNum = new Regex("^[A-Za-z0-9_-]*$");
+
+        private void textBox_player1_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = _lettNum.IsMatch(e.Text);
+        }
+
+        private void textBox_player2_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = _lettNum.IsMatch(e.Text);
+        }
+    }
+}
