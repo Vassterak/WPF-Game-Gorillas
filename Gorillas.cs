da03f5c89897adc42270da3bc52524934f46c6a8
Vassterak <vaclavhorcicka@outlook.cz>
2021-11-11 17:09:58 +0100

    Random players position generation

--- Gorillas.cs
+++ Gorillas.cs
@@ -22,8 +22,7 @@ namespace WPF_Game_Gorillas
         {
             this.gameGrid = gameGrid;
             PrepareGameField(windowWidth, windowHeight);
-            RenderSkyscrapers();
-            GorillaSpawn();
+            RenderSkyscrapers(); //w GorillaSpawn
         }
 
         private void PrepareGameField(double windowWidth, double windowHeight) //Create a grid layout for the content
@@ -44,8 +43,10 @@ namespace WPF_Game_Gorillas
             }
         }
 
-        private void RenderSkyscrapers()
+        private void RenderSkyscrapers() 
         {
+            int[] gorillasLocation = GorillasLocation(); //generate random column position for gorillas (players)
+
             for (int x = 0; x < GridColumns; x++)
             {
                 int skyscraperHeight = rnd.Next(2, (int)GridRows - 5);
@@ -55,17 +56,31 @@ namespace WPF_Game_Gorillas
                 rectangle.SetValue(Grid.ColumnProperty, x);
                 Grid.SetRowSpan(rectangle,skyscraperHeight + 1);
                 gameGrid.Children.Add(rectangle);
+
+                if (gorillasLocation[0] == x || gorillasLocation[1] == x) //when position is met add player.
+                    GorillaSpawn(x, (int)GridRows - skyscraperHeight - 3);
             }
         }
 
-        private void GorillaSpawn()
+        private int[] GorillasLocation()
+        {
+            int[] player = new int[2];
+            player[0] = rnd.Next(0, 3);
+            player[1] = rnd.Next((int)GridColumns - 3, (int)GridColumns);
+
+            return player;
+        }
+
+        private void GorillaSpawn(int gridColumn, int rowHeight)
         {
             Rectangle gorillaSprite = new Rectangle();
             gorillaSprite.Fill = new ImageBrush(new BitmapImage(new Uri("Resources/gorilla.png", UriKind.Relative))); //Microsoft's horrible image implementation
 
-            gorillaSprite.SetValue(Grid.RowProperty, 0);
-            gorillaSprite.SetValue(Grid.ColumnProperty, 0);
+            gorillaSprite.SetValue(Grid.RowProperty, rowHeight);
+            gorillaSprite.SetValue(Grid.ColumnProperty, gridColumn);
+            Grid.SetRowSpan(gorillaSprite, 3);
             gameGrid.Children.Add(gorillaSprite);
+
         }
     }
 }
